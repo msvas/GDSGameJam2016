1e151966f4c950b4dce609b0743a33b281774e20
Marcelo Vasques <Marcelo Vasques>
2016-08-19 11:53:05 -0300

    Planet script

---
+++ Assets/Planet/Scripts/Planet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+    [Range(-10, 10)]
+    public int gravity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider other) {
+        float direction = Vector3.Normalize(Vector3.Distance(other.transform.position, transform.position));
+        other.gameObject.GetComponent<Rigidbody>().AddForce(Direction.TransformDirection(distance * Forward_Speed * Time.deltaTime));
+    }
+}
