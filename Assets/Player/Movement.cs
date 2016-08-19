e93bd5886500eafea175cc0f5da545208a650ed3
Khin Baptista <khin.baptista@gmail.com>
2016-08-19 13:25:57 -0300

    Added movement in space + ground check
    UP NEXT
    - When the player is on the ground, make it stand on the planet (depends
      on direction of the planet)
    - Add movement on planet (cw, ccw)
    - Add jump

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -13,16 +13,53 @@ public class Movement : MonoBehaviour {
 	private string JumpInput = "Jump";
 
 	private Player player;
+	private Rigidbody rb;
 
 	void Start () {
-		player = GetComponent<Player>();
+		player	= GetComponent<Player>();
+		rb		= GetComponent<Rigidbody>();
 	}
 	
 	void Update () {
-		
+		HandleInput();		
 	}
 
-	void HandleInput() {
+	void HandleInput () {
+		float horizontal	= Input.GetAxis(HorizontalInput);
+		float vertical		= Input.GetAxis(VerticalInput);
+		bool jump			= Input.GetAxis(JumpInput) > 0;
 
+		if (player.isGrounded()) {
+
+			if (jump) {
+
+			}
+
+			if (horizontal != 0)
+				MoveClockwise(horizontal);
+			else if (vertical != 0)
+				MoveCounterClockwise(vertical);
+			
+		}
+		else {
+
+			MoveInSpace(new Vector2(horizontal, vertical));
+
+		}
+	}
+
+	void MoveClockwise (float input) {
+
+	}
+
+	void MoveCounterClockwise (float input) {
+
+	}
+
+	void MoveInSpace (Vector2 input) {
+		Vector2 normalized = input.normalized;
+		Vector3 movement = new Vector3(normalized.x, normalized.y, 0.0f);
+
+		rb.AddForce(movement);
 	}
 }
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -9,10 +9,22 @@ public class Player : MonoBehaviour {
 	public void setGrounded (bool grounded) { this.grounded = grounded; }
 
 	void Start () {
-	
+		
 	}
 	
 	void Update () {
-	
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer("Planet")) {
+			grounded = true;
+		}
+	}
+
+	void OnTriggerLeave(Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer("Planet")) {
+			grounded = false;
+		}
 	}
 }
