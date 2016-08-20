eb9781671895b206c178fbda3fdd86c4f1f1e4ad
Khin Baptista <khin.baptista@gmail.com>
2016-08-20 03:39:37 -0300

    Added diamond planet + more planets into generator

---
+++ Assets/Gameplay/FinishLine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+			// Player has reached the end
+
+			SceneManager.LoadScene("Stage");
+		}
+	}
+}
