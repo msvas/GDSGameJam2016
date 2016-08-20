f25d93a508c46a17da7ca1232066fa5ae2a082b9
Khin Baptista <khin.baptista@gmail.com>
2016-08-20 09:07:45 -0300

    Camera follows both players in orthographic mode

--- Assets/Gameplay/CameraControl.cs
+++ Assets/Gameplay/CameraControl.cs
@@ -4,17 +4,41 @@ using System.Collections;
 public class CameraControl : MonoBehaviour {
 
 	[SerializeField]
-	private Transform player;
+	private Transform player1;
+
+	[SerializeField]
+	private Transform player2;
+
+	[SerializeField]
+	private float minSizeY;
+
+	private new Camera camera;
 
 	void Start () {
-	
+		camera = GetComponent<Camera>();
 	}
 	
 	void Update () {
-		Vector3 position = transform.position;
-		position.x = player.position.x;
-		position.y = player.position.y;
+		SetCameraPosition();
+		SetCameraSize();
+	}
 
-		transform.position = position;
+	void SetCameraPosition() {
+		Vector3 middle = (player1.position + player2.position) * 0.5f;
+		camera.transform.position = new Vector3(middle.x, middle.y, camera.transform.position.z);
+	}
+
+	void SetCameraSize() {
+		//horizontal size is based on actual screen ratio
+		float minSizeX = minSizeY * Screen.width / Screen.height;
+
+		//multiplying by 0.5, because the ortographicSize is actually half the height
+		float width = (Mathf.Abs(player1.position.x - player2.position.x) + 5) * 0.5f;
+		float height = (Mathf.Abs(player1.position.y - player2.position.y) + 5 )* 0.5f;
+
+		//computing the size
+		float camSizeX = Mathf.Max(width, minSizeX);
+		camera.orthographicSize = Mathf.Max(height,
+			 camSizeX * Screen.height / Screen.width, minSizeY);
 	}
 }
