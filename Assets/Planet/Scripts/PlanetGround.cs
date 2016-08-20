785688913daa89f3bc8f442fe816b974c974cd26
Khin Baptista <khin.baptista@gmail.com>
2016-08-20 07:02:36 -0300

    Added spin to planets

--- Assets/Planet/Scripts/PlanetGround.cs
+++ Assets/Planet/Scripts/PlanetGround.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlanetGround : MonoBehaviour {
 
+	[SerializeField]
+	private float spinSpeed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,6 @@ public class PlanetGround : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate(new Vector3(0.0f, 0.0f, spinSpeed * Time.deltaTime));
 	}
 }
