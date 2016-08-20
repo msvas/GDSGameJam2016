3f24e0a51bbb15c10bb0a50c320e073b4a5b178f
Khin Baptista <khin.baptista@gmail.com>
2016-08-19 21:36:50 -0300

    Created diamond (finish line) planet

---
+++ Assets/Gameplay/FinishLine.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			SceneManager.LoadScene("Stage");
+		}
+	}
+
+}
