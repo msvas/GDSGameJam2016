7bea2509fa59a0332fe0de420ac254b1a09b06e2
Marcelo Vasques <msouvas@gmail.com>
2016-08-20 01:40:40 -0300

    Black hole resets the scene

---
+++ Assets/Items/Scripts/BlackHole.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BlackHole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coll) {
+        Debug.Log("oi");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
