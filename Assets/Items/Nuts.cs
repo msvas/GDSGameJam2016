93d1c457d7d95d53681c880090990e48a5dfd777
Marcelo Vasques <msouvas@gmail.com>
2016-08-19 21:26:29 -0300

    Nut powerup added

---
+++ Assets/Items/Nuts.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nuts : MonoBehaviour {
+
+    private int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(transform.parent.position, Vector3.back, speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            //other.gameObject.GetComponent<Player>.ActivateNut();
+            Object.Destroy(this);
+        }
+    }
+}
