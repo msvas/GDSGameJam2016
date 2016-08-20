6e58615d97837b499dd4d020901c00749503ba39
Marcelo Vasques <msouvas@gmail.com>
2016-08-20 13:20:53 -0300

    Stardust added

---
+++ Assets/Stardust.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stardust : MonoBehaviour {
+
+    private int speed = 60;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            other.gameObject.GetComponent<Player>().ActivateStar();
+        }
+    }
+}
